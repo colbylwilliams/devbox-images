4de9c16f8b50e39f3cfd3f608d344699724b124a
Colby Williams <ColbyLWilliams@gmail.com>
2022-07-14 16:40:02 -0500

    Create Install-AzPsModule.ps1

---
+++ scripts/Install-AzPsModule.ps1
@@ -0,0 +1,16 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# Set TLS1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
+
+# # The correct Modules need to be saved in C:\Modules
+# $installPSModulePath = "C:\\Modules"
+# if (-not (Test-Path -LiteralPath $installPSModulePath))
+# {
+#     Write-Host "Creating ${installPSModulePath} folder to store PowerShell Azure modules..."
+#     $null = New-Item -Path $installPSModulePath -ItemType Directory
+# }
+
+Write-Host "Installing AZ to the ${installPSModulePath} path..."
+Install-Module -Name Az -Scope AllUsers -SkipPublisherCheck -Force
